07be1c250a294b127d13e4d85d04c92c5258d12c
roshhni97 <roshani.navdiya07@gmail.com>
2024-04-14 15:31:44 +0530

    init: watcher details page

---
+++ src/app/watcher/details/[id]/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import Navbar from "@/components/common/Navbar";
+import LeftArrow from "@/assets/left-arrow.svg";
+import { Switch } from "@/components/ui/switch";
+import test from "@/assets/test.png";
+import Image from "next/image";
+import { title } from "process";
+
+const TableData = [
+  {
+    title: "Link",
+    value: "https://vercel.com/rajvarsanis-projects/DEMO-APP/analytics",
+  },
+  {
+    title: "Alert",
+    value: "Spike > 50% in a day",
+  },
+  {
+    title: "Hit Interval",
+    value: "10 Mins",
+  },
+  {
+    title: "Status",
+    value: "true",
+  },
+];
+
+const Stats = {
+  successfulHits: 500,
+  failedHits: 4,
+  altersSent: 9,
+};
+
+const Row = ({ title, value }: { title: string; value: string }) => {
+  return (
+    <tr className="">
+      <td className="opacity-40 uppercase pr-5">{title}:</td>
+      {title === "Status" ? (
+        <td>
+          <Switch className="rounded-0" />
+        </td>
+      ) : (
+        <td className="uppercase">{value}</td>
+      )}
+    </tr>
+  );
+};
+
+const Details = () => {
+  return (
+    <div className="flex flex-col gap-10">
+      <Navbar />
+      <div className="flex gap-2 items-center">
+        <LeftArrow />
+        <h1 className="text-4xl uppercase">Watcher Details</h1>
+      </div>
+      <div className="bg-[#0E1A1E] w-full border-2 p-10 border-[#2E5050]">
+        <table>
+          <tbody className="">
+            {TableData.map((data, index) => (
+              <Row key={index} title={data.title} value={data.value} />
+            ))}
+          </tbody>
+        </table>
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-4">
+            <div className="opacity-40 uppercase">Snapshot:</div>
+            <Image src={test} alt="test" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="opacity-40 uppercase">Stats:</div>
+            <div>
+              <li className="uppercase">
+                ✅ {Stats.successfulHits} Successful hits
+              </li>
+              <li className="uppercase">🚨 {Stats.failedHits} Failed hits</li>
+              <li className="uppercase">
+                🚨 {Stats.successfulHits} Alerts Sent
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
---
+++ src/assets/left-arrow.svg.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={36}
+    height={22}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#DAF9F2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={4.333}
+      d="M2.833 11h30.334M2.833 11l8.667 8.667M2.833 11 11.5 2.333"
+    />
+  </svg>
+);
+export default Icon;

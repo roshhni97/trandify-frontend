420c4612af96856a6192bdf3e44260164daad4a2
roshhni97 <roshani.navdiya07@gmail.com>
2024-05-03 03:06:38 +0530

    Add private paths to AuthProvider

--- src/components/common/AuthProvider.tsx
+++ src/components/common/AuthProvider.tsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 const publicPaths = ["/"];
 const publicHomePath = "/";
 const privateHomePath = "/dashboard";
+const privatePaths = [privateHomePath, "/watcher"];
 
 const AuthProvider = ({ children } : {
     children: React.ReactNode;
@@ -19,11 +20,13 @@ const AuthProvider = ({ children } : {
     useEffect(() => {
       if (!isUserDataLoading) {
         if (errorFetchingUserData || !userData) {
+          // Redirect to public home if user is not logged in and trying to access private paths
           if (!publicPaths.includes(pathname)) {
             router.push(publicHomePath);
           }
         } else {
-          if (!pathname.startsWith(privateHomePath)) {
+          // Redirect to private home if user is logged in and trying to access public paths
+          if (!pathname.startsWith(privatePaths[0]) && !privatePaths.includes(pathname) ) {
             router.push(privateHomePath);
           }
         }

3cb0b95d6d78a3afe468296fbc64296323604579
roshhni97 <roshani.navdiya07@gmail.com>
2024-05-02 21:23:21 +0530

    Refactor image utils and add image cropping functionality to watcher/add page

--- src/app/watcher/add/page.tsx
+++ src/app/watcher/add/page.tsx
@@ -8,6 +8,11 @@ import ReactCrop, { type Crop } from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 import { useToast } from "@/components/ui/use-toast";
 
+const imgUrl =
+  "https://cdn.discordapp.com/attachments/1228726876013596756/1229053715215028386/661bd11286865d45cbfca821.png?ex=662e4878&is=661bd378&hm=751183956caf2f167b9dd2b43b48dd0df940cf5ac1e42056c0983b3da8cd4243&";
+// const imgUrl =
+//   "https://genai-trendify-bucket.s3.eu-north-1.amazonaws.com/661bb4ea2358035a9ed82387/661bd65fdcb583aaa4ee7200.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFQaCmV1LW5vcnRoLTEiRzBFAiEAukkcZdd%2F%2BxLQAc9r7wXdnMxdyR6qXoBlCPojGISlSKICIETLK61DqfVPVhAic8mza9hfPb94aHT0gAlsYi97eVXJKu0CCI7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQARoMMjUzMDQ3MTMwMTcwIgwsideIIOIEqWoFZXkqwQJLdBFRPXe17o%2Bwsr%2FI%2Fio5y4uTyFqMs1zZ1in%2B1WR5BdTZzy%2B2c16n0IBUckumAeiPYX1f9J6KM1QUgpevdFLpbKUEwmQ93FJfa4lP%2FdJoX%2Fd%2FnU1UZh9i%2FyfMOY4CHHcM58hN5ENwPDpsSuN9cUcTla0HqBqMy7sOoMyJPB2LyToajVvuUiDRW1EvLHAM%2BGWOaBIJYRjiuQu24%2F%2BpO9TNfo8nDD%2F5YVh7lZYOs6kXJZufiQ7WKh%2BpoWdunRzG6ZkvMQ19kVh7BbQienUkyGnaGTFpZ2Mt0NEm8IypDJwt5IXAiRu2IfpyaBrALCpbvdiG4z6MTblFGXuwvY0VrSuA%2BSVK0DDd83I2vayvYljpHNBGpluX6yh624VtpQjWsv%2BT6Rl1yLKwxkd8%2FmrzfdVB%2BSB7fsyPjxLDCH6mykYU1yYw55XvsAY6swKFOVbjvZyHBUwd5%2FKOpHzl0QnrKys8qJ93nmBjA6%2FYHD0YD%2Fbp9oYnxeaHrrbwMOyPOkX7Oq5MMaZJv41gFDo5Pr8vLcAcFLkYo2pyJO0aC%2FKr1c%2FvGhEHUn2zjNIs%2Brxk8BXb28Whq993pUEnZDtlTidaZDC4jbYGII7Nmsqn21V5G4E4VHLMAPhwiM%2FUCVehdjJKufnUUJcO%2FvTxwzrI4Cypr8%2BS%2BXWpbEkCobBbEDNmkW4oFOeYKvT2Q09D6GTDxl0t%2FO%2FnF1v6K0kaI7j8IUqc8qip4HS2pAXbqUlueTOr0%2BR3QtIyj061ZNNM2IrH69svCCKIGA%2FygNiCT31EBEPyGffmHUIwIGisZ5GkQv8CUu3t6XAg1yUnxGQFEGPvR3gJeZr9bS%2Bie63sbSGhOYoR&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240414T154619Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43199&X-Amz-Credential=ASIATV2WIZA5B4PR3NCJ%2F20240414%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=9c1cef1850d6cbce609de9ae0e67433e325ede9535a17739b9258ed54cfedf6c";
+
 const Details = () => {
   const { toast } = useToast();
 
@@ -120,12 +125,7 @@ const Details = () => {
                   position: "relative",
                 }}
               >
-                <img
-                  src={
-                    "https://genai-trendify-bucket.s3.eu-north-1.amazonaws.com/661bb4ea2358035a9ed82387/661bd65fdcb583aaa4ee7200.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFQaCmV1LW5vcnRoLTEiRzBFAiEAukkcZdd%2F%2BxLQAc9r7wXdnMxdyR6qXoBlCPojGISlSKICIETLK61DqfVPVhAic8mza9hfPb94aHT0gAlsYi97eVXJKu0CCI7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQARoMMjUzMDQ3MTMwMTcwIgwsideIIOIEqWoFZXkqwQJLdBFRPXe17o%2Bwsr%2FI%2Fio5y4uTyFqMs1zZ1in%2B1WR5BdTZzy%2B2c16n0IBUckumAeiPYX1f9J6KM1QUgpevdFLpbKUEwmQ93FJfa4lP%2FdJoX%2Fd%2FnU1UZh9i%2FyfMOY4CHHcM58hN5ENwPDpsSuN9cUcTla0HqBqMy7sOoMyJPB2LyToajVvuUiDRW1EvLHAM%2BGWOaBIJYRjiuQu24%2F%2BpO9TNfo8nDD%2F5YVh7lZYOs6kXJZufiQ7WKh%2BpoWdunRzG6ZkvMQ19kVh7BbQienUkyGnaGTFpZ2Mt0NEm8IypDJwt5IXAiRu2IfpyaBrALCpbvdiG4z6MTblFGXuwvY0VrSuA%2BSVK0DDd83I2vayvYljpHNBGpluX6yh624VtpQjWsv%2BT6Rl1yLKwxkd8%2FmrzfdVB%2BSB7fsyPjxLDCH6mykYU1yYw55XvsAY6swKFOVbjvZyHBUwd5%2FKOpHzl0QnrKys8qJ93nmBjA6%2FYHD0YD%2Fbp9oYnxeaHrrbwMOyPOkX7Oq5MMaZJv41gFDo5Pr8vLcAcFLkYo2pyJO0aC%2FKr1c%2FvGhEHUn2zjNIs%2Brxk8BXb28Whq993pUEnZDtlTidaZDC4jbYGII7Nmsqn21V5G4E4VHLMAPhwiM%2FUCVehdjJKufnUUJcO%2FvTxwzrI4Cypr8%2BS%2BXWpbEkCobBbEDNmkW4oFOeYKvT2Q09D6GTDxl0t%2FO%2FnF1v6K0kaI7j8IUqc8qip4HS2pAXbqUlueTOr0%2BR3QtIyj061ZNNM2IrH69svCCKIGA%2FygNiCT31EBEPyGffmHUIwIGisZ5GkQv8CUu3t6XAg1yUnxGQFEGPvR3gJeZr9bS%2Bie63sbSGhOYoR&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240414T154619Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43199&X-Amz-Credential=ASIATV2WIZA5B4PR3NCJ%2F20240414%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=9c1cef1850d6cbce609de9ae0e67433e325ede9535a17739b9258ed54cfedf6c"
-                  }
-                  alt="test"
-                />
+                <img src={imgUrl} alt="test" />
               </ReactCrop>
             </div>
             <div className="flex gap-2">
--- src/app/watcher/editor/[id]/page.tsx
+++ src/app/watcher/editor/[id]/page.tsx
@@ -6,62 +6,18 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import ReactCrop, { type Crop } from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
+import { getElementRects, convertCoordinates } from "@/utils/image.utils";
 
 const dummyData = {
   link: "https://vercel.com/rajvarsanis-projects/DEMO-APP/analytics",
   alert: "Spike > 50% in a day",
   hitInterval: 10,
   status: true,
+  // image:
+  //   "https://genai-trendify-bucket.s3.eu-north-1.amazonaws.com/661bb4ea2358035a9ed82387/661bd65fdcb583aaa4ee7200.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFQaCmV1LW5vcnRoLTEiRzBFAiEAukkcZdd%2F%2BxLQAc9r7wXdnMxdyR6qXoBlCPojGISlSKICIETLK61DqfVPVhAic8mza9hfPb94aHT0gAlsYi97eVXJKu0CCI7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQARoMMjUzMDQ3MTMwMTcwIgwsideIIOIEqWoFZXkqwQJLdBFRPXe17o%2Bwsr%2FI%2Fio5y4uTyFqMs1zZ1in%2B1WR5BdTZzy%2B2c16n0IBUckumAeiPYX1f9J6KM1QUgpevdFLpbKUEwmQ93FJfa4lP%2FdJoX%2Fd%2FnU1UZh9i%2FyfMOY4CHHcM58hN5ENwPDpsSuN9cUcTla0HqBqMy7sOoMyJPB2LyToajVvuUiDRW1EvLHAM%2BGWOaBIJYRjiuQu24%2F%2BpO9TNfo8nDD%2F5YVh7lZYOs6kXJZufiQ7WKh%2BpoWdunRzG6ZkvMQ19kVh7BbQienUkyGnaGTFpZ2Mt0NEm8IypDJwt5IXAiRu2IfpyaBrALCpbvdiG4z6MTblFGXuwvY0VrSuA%2BSVK0DDd83I2vayvYljpHNBGpluX6yh624VtpQjWsv%2BT6Rl1yLKwxkd8%2FmrzfdVB%2BSB7fsyPjxLDCH6mykYU1yYw55XvsAY6swKFOVbjvZyHBUwd5%2FKOpHzl0QnrKys8qJ93nmBjA6%2FYHD0YD%2Fbp9oYnxeaHrrbwMOyPOkX7Oq5MMaZJv41gFDo5Pr8vLcAcFLkYo2pyJO0aC%2FKr1c%2FvGhEHUn2zjNIs%2Brxk8BXb28Whq993pUEnZDtlTidaZDC4jbYGII7Nmsqn21V5G4E4VHLMAPhwiM%2FUCVehdjJKufnUUJcO%2FvTxwzrI4Cypr8%2BS%2BXWpbEkCobBbEDNmkW4oFOeYKvT2Q09D6GTDxl0t%2FO%2FnF1v6K0kaI7j8IUqc8qip4HS2pAXbqUlueTOr0%2BR3QtIyj061ZNNM2IrH69svCCKIGA%2FygNiCT31EBEPyGffmHUIwIGisZ5GkQv8CUu3t6XAg1yUnxGQFEGPvR3gJeZr9bS%2Bie63sbSGhOYoR&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240414T154619Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43199&X-Amz-Credential=ASIATV2WIZA5B4PR3NCJ%2F20240414%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=9c1cef1850d6cbce609de9ae0e67433e325ede9535a17739b9258ed54cfedf6c",
   image:
-    "https://genai-trendify-bucket.s3.eu-north-1.amazonaws.com/661bb4ea2358035a9ed82387/661bd65fdcb583aaa4ee7200.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFQaCmV1LW5vcnRoLTEiRzBFAiEAukkcZdd%2F%2BxLQAc9r7wXdnMxdyR6qXoBlCPojGISlSKICIETLK61DqfVPVhAic8mza9hfPb94aHT0gAlsYi97eVXJKu0CCI7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQARoMMjUzMDQ3MTMwMTcwIgwsideIIOIEqWoFZXkqwQJLdBFRPXe17o%2Bwsr%2FI%2Fio5y4uTyFqMs1zZ1in%2B1WR5BdTZzy%2B2c16n0IBUckumAeiPYX1f9J6KM1QUgpevdFLpbKUEwmQ93FJfa4lP%2FdJoX%2Fd%2FnU1UZh9i%2FyfMOY4CHHcM58hN5ENwPDpsSuN9cUcTla0HqBqMy7sOoMyJPB2LyToajVvuUiDRW1EvLHAM%2BGWOaBIJYRjiuQu24%2F%2BpO9TNfo8nDD%2F5YVh7lZYOs6kXJZufiQ7WKh%2BpoWdunRzG6ZkvMQ19kVh7BbQienUkyGnaGTFpZ2Mt0NEm8IypDJwt5IXAiRu2IfpyaBrALCpbvdiG4z6MTblFGXuwvY0VrSuA%2BSVK0DDd83I2vayvYljpHNBGpluX6yh624VtpQjWsv%2BT6Rl1yLKwxkd8%2FmrzfdVB%2BSB7fsyPjxLDCH6mykYU1yYw55XvsAY6swKFOVbjvZyHBUwd5%2FKOpHzl0QnrKys8qJ93nmBjA6%2FYHD0YD%2Fbp9oYnxeaHrrbwMOyPOkX7Oq5MMaZJv41gFDo5Pr8vLcAcFLkYo2pyJO0aC%2FKr1c%2FvGhEHUn2zjNIs%2Brxk8BXb28Whq993pUEnZDtlTidaZDC4jbYGII7Nmsqn21V5G4E4VHLMAPhwiM%2FUCVehdjJKufnUUJcO%2FvTxwzrI4Cypr8%2BS%2BXWpbEkCobBbEDNmkW4oFOeYKvT2Q09D6GTDxl0t%2FO%2FnF1v6K0kaI7j8IUqc8qip4HS2pAXbqUlueTOr0%2BR3QtIyj061ZNNM2IrH69svCCKIGA%2FygNiCT31EBEPyGffmHUIwIGisZ5GkQv8CUu3t6XAg1yUnxGQFEGPvR3gJeZr9bS%2Bie63sbSGhOYoR&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240414T154619Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43199&X-Amz-Credential=ASIATV2WIZA5B4PR3NCJ%2F20240414%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=9c1cef1850d6cbce609de9ae0e67433e325ede9535a17739b9258ed54cfedf6c",
+    "https://cdn.discordapp.com/attachments/1228726876013596756/1229053715215028386/661bd11286865d45cbfca821.png?ex=662e4878&is=661bd378&hm=751183956caf2f167b9dd2b43b48dd0df940cf5ac1e42056c0983b3da8cd4243&",
 };
-
-function getElementRects(
-  elementOrSelector:
-    | {
-        getBoundingClientRect: () => { height: number; width: number };
-      }
-    | string
-) {
-  const element =
-    typeof elementOrSelector === "string"
-      ? document.querySelector(elementOrSelector)
-      : elementOrSelector;
-
-  if (!element) {
-    console.error("Element not found.");
-    return null;
-  }
-
-  const rect = element.getBoundingClientRect();
-  return {
-    height: rect.height,
-    width: rect.width,
-  };
-}
-
-function convertCoordinates({
-  currentWidth,
-  currentHeight,
-  x,
-  y,
-}: {
-  currentWidth: number;
-  currentHeight: number;
-  x: number;
-  y: number;
-}) {
-  const baseWidth = 1440;
-  const baseHeight = 900;
-
-  const scaleFactorWidth = baseWidth / currentWidth;
-  const scaleFactorHeight = baseHeight / currentHeight;
-
-  const newX = x * scaleFactorWidth;
-  const newY = y * scaleFactorHeight;
-
-  return { newX, newY };
-}
 
 const Details = () => {
   const data = dummyData;
